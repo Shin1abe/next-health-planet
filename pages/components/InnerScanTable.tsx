9c0a564910fd336cd6720548d15b3051cc358b1f
Shin1 <shin1abewk@gmail.com>
2023-06-03 07:39:10 +0900

    getServerSideProps変更

--- pages/components/InnerScanTable.tsx
+++ pages/components/InnerScanTable.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { GetServerSideProps } from 'next'
-import { getServerSideProps } from '../getData'
 import axios from 'axios'
 import querystring from 'query-string'
 import moment from 'moment'
@@ -37,12 +36,6 @@ export type Props = {
   sex?: string
   birth_date?: string
   datas?: InnerScanData[]
-}
-const formatDate = (yyyymmdd: string): string => {
-  const year = yyyymmdd?.slice(0, 4)
-  const month = yyyymmdd?.slice(4, 6)
-  const day = yyyymmdd?.slice(6, 8)
-  return `${year}/${month}/${day}`
 }
 const InnerScanTable = (props: Props) => {
   console.log('InnerScanTable')
@@ -144,86 +137,86 @@ const InnerScanTable = (props: Props) => {
 export default InnerScanTable
 //------------------------------------------------------------------------
 // サーバサイドで実行する処理(getServerSideProps)を定義
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   // APIやDBからのデータ取得処理などを記載
-//   const accessToken = context.query.access_token
-//   const today = moment().format('YYYYMMDDHHmmss')
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // APIやDBからのデータ取得処理などを記載
+  const accessToken = context.query.access_token
+  const today = moment().format('YYYYMMDDHHmmss')
 
-//   // innerscanデータ取得
-//   //apiへのリクエストパラメタ（6021 : 体重 (kg)、6022 : 体脂肪率 (%)）
-//   const insRequestBody = {
-//     access_token: accessToken,
-//     date: 1,
-//     to: today,
-//     tag: '6021,6022',
-//   }
+  // innerscanデータ取得
+  //apiへのリクエストパラメタ（6021 : 体重 (kg)、6022 : 体脂肪率 (%)）
+  const insRequestBody = {
+    access_token: accessToken,
+    date: 1,
+    to: today,
+    tag: '6021,6022',
+  }
 
-//   let innerscanRes: any
-//   if (accessToken) {
-//     await axios
-//       .post(
-//         'https://www.healthplanet.jp/status/innerscan.json',
-//         querystring.stringify(insRequestBody),
-//         {
-//           headers: {
-//             'Content-Type': 'application/x-www-form-urlencoded',
-//           },
-//         }
-//       )
-//       .then((response) => {
-//         innerscanRes = response
-//       })
-//       .catch((error) => {
-//         console.error(error)
-//       })
-//   }
+  let innerscanRes: any
+  if (accessToken) {
+    await axios
+      .post(
+        'https://www.healthplanet.jp/status/innerscan.json',
+        querystring.stringify(insRequestBody),
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        }
+      )
+      .then((response) => {
+        innerscanRes = response
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
 
-//   // pedometerデータ取得
-//   //apiへのリクエストパラメタ（6631 : 歩数（歩））
-//   const pedRequestBody = {
-//     access_token: accessToken,
-//     date: 1,
-//     to: today,
-//     tag: '6631',
-//   }
+  // pedometerデータ取得
+  //apiへのリクエストパラメタ（6631 : 歩数（歩））
+  const pedRequestBody = {
+    access_token: accessToken,
+    date: 1,
+    to: today,
+    tag: '6631',
+  }
 
-//   let pedRes: any
-//   if (accessToken) {
-//     await axios
-//       .post(
-//         'https://www.healthplanet.jp/status/pedometer.json',
-//         querystring.stringify(pedRequestBody),
-//         {
-//           headers: {
-//             'Content-Type': 'application/x-www-form-urlencoded',
-//           },
-//         }
-//       )
-//       .then((response) => {
-//         pedRes = response
-//       })
-//       .catch((error) => {
-//         console.error(error)
-//       })
-//   }
+  let pedRes: any
+  if (accessToken) {
+    await axios
+      .post(
+        'https://www.healthplanet.jp/status/pedometer.json',
+        querystring.stringify(pedRequestBody),
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        }
+      )
+      .then((response) => {
+        pedRes = response
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
 
-//   // innerscanResとpemeterの結果をmerge
-//   const mergedArr = [...innerscanRes.data.data, ...pedRes.data.data].reduce(
-//     (acc, curr) => {
-//       acc.push(curr)
-//       return acc
-//     },
-//     []
-//   )
+  // innerscanResとpemeterの結果をmerge
+  const mergedArr = [...innerscanRes.data.data, ...pedRes.data.data].reduce(
+    (acc, curr) => {
+      acc.push(curr)
+      return acc
+    },
+    []
+  )
 
-//   const props: Props = {
-//     height: innerscanRes?.data?.height || null,
-//     sex: innerscanRes?.data?.sex || null,
-//     birth_date: innerscanRes?.data?.birth_date || null,
-//     datas: mergedArr || null,
-//   }
+  const props: Props = {
+    height: innerscanRes?.data?.height || null,
+    sex: innerscanRes?.data?.sex || null,
+    birth_date: innerscanRes?.data?.birth_date || null,
+    datas: mergedArr || null,
+  }
 
-//   return {
-//     props: props,
-//   }
-// }
+  return {
+    props: props,
+  }
+}
